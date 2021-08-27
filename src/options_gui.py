4fe12d762960826b0504a86f19057a36f5ca7cfe
Marc Feldman <marcryanfeldman@me.com>
2021-08-26 17:24:49 -0700

    simplified row/column configure grid

--- src/options_gui.py
+++ src/options_gui.py
@@ -3,15 +3,13 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 import tkinter as tk
 
-# -------------------------- INPUTS --------------------------
-column_names = ["Stock Symbol","Expiry","Type","Buy/Sell",\
-                "Open\nPrice/Share","# Shares",\
-                "Current\nPrice/Share","Strike","Premium",\
-                "# Contracts","Close\nStrategy",\
-                "Close\nPrice/Share","Close\nPremium"\
-                "Fee","Profit/Loss",\
-                "Net Realized P/L\nFor Underlier",\
-                "Status","Account"]
+# ----------------------------------------- INPUTS -----------------------------------------
+column_names = ["Stock Symbol","Expiry","Type","Buy/Sell","Open\nPrice/Share","# Shares",\
+                "Current\nPrice/Share","Strike","Premium","# Contracts","Close\nStrategy",\
+                "Close\nPrice/Share","Close\nPremium","Fee","Profit/Loss",\
+                "Net Realized P/L\nFor Underlier","Status","Account"]
+maxColumns = 26
+maxRows = 30
 # ------------------------------------------------------------
 
 window = tk.Tk()
@@ -22,9 +20,9 @@ height= window.winfo_screenheight()
 window.geometry("%dx%d" % (width, height))
 window.title("Options Positions")
 
-# configure grid
-window.columnconfigure((0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18), weight=1)
-window.rowconfigure((0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20), weight=1)
+# configure grid cells
+[window.columnconfigure(i, weight=1) for i in range(maxColumns)]
+[window.rowconfigure(i, weight=1) for i in range(maxRows)]
 
 # Create column headers
 seriesOffset = 11
@@ -34,4 +32,8 @@ for label in column_names:
                             font='"Helvetica Neue" 14 bold')
     columnLabel.grid(column=col, row=seriesOffset)
     col=col+1
+
+window.lift()
+window.attributes('-topmost',True)
+window.after_idle(window.attributes,'-topmost',False)
 window.mainloop()
