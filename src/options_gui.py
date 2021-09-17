d9b71af900676371cff98984d7c8532fdf2604f6
Marc Feldman <marcryanfeldman@me.com>
2021-09-16 21:52:47 -0700

    using PySimpleGUI

--- src/options_gui.py
+++ src/options_gui.py
@@ -1,46 +1,24 @@
 # imports
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
-import tkinter as tk
+import PySimpleGUI as sg
 
 # ----------------------------------------- INPUTS -----------------------------------------
 column_names = ["Stock Symbol"]
-maxColumns = 10
-maxRows = 10
 # ------------------------------------------------------------
-# derive parameters
-newEntryButtonOffset = 3
-seriesOffset = newEntryButtonOffset + 1
 
-# create scrollable window
-window = tk.Tk()
+sg.theme('DarkAmber')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('      '), sg.Text(column_names[0])],
+            [sg.Button('+'), sg.InputText()] ]
 
-# get width x height of display
-width= window.winfo_screenwidth() 
-height= window.winfo_screenheight()
-window.geometry("%dx%d" % (width, height))
-window.title("Options Positions")
+# Create the Window
+window = sg.Window('Window Title', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
 
-# configure grid cells
-[window.columnconfigure(i, weight=1) for i in range(maxColumns)]
-[window.rowconfigure(i, weight=1) for i in range(maxRows)]
-
-# Create column headers
-col = 0
-for label in column_names:
-    columnLabel = tk.Label( text=label,\
-                            font='"Helvetica Neue" 14 bold')
-    columnLabel.grid(column=col, row=seriesOffset)
-    col=col+1
-
-# create "new entry" fields
-col = 0
-for j in range(1):
-    addEntryButton = tk.Button(window, text="+ Entry")
-    addEntryButton.grid(column=col, row=newEntryButtonOffset)
-    col = col + 1
-
-window.lift()
-window.attributes('-topmost',True)
-window.after_idle(window.attributes,'-topmost',False)
-window.mainloop()
+window.close()
